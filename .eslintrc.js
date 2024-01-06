586c4519455aebdd602d20aff0543ba7ddda8611
benjaminjkern <benkern@pixelgate.net>
2024-01-06 11:58:40 -0800

    Pawn rules and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    extends: ["eslint:recommended"],
+    parserOptions: {
+        sourceType: "module",
+        ecmaVersion: "latest",
+    },
+    env: {
+        browser: true,
+        commonjs: true,
+        es6: true,
+        node: true,
+    },
+    ignorePatterns: ["**/*.html"],
+};
--- game.js
+++ game.js
@@ -44,7 +44,44 @@ export const setupBoardControls = (boardPieces, redraw) => {
     };
 };
 
-const validMove = (fromSpace, toSpace) => {
+const validMove = (fromSpace, toSpace, boardPieces) => {
     if (toSpace.boardPiece[0] === fromSpace.boardPiece[0]) return false;
+    const yDiff = toSpace.boardY - fromSpace.boardY;
+    const xDiff = toSpace.boardX - fromSpace.boardX;
+    switch (fromSpace.boardPiece[1]) {
+        case "P":
+            if (fromSpace.boardPiece[0] === "W") {
+                // 6 is HARDCODED WONT WORK WITH DIFFERENT SIZED BOARDS
+                if (
+                    xDiff === 0 &&
+                    (yDiff === -1 ||
+                        (fromSpace.boardY === 6 && yDiff === -2)) &&
+                    toSpace.boardPiece === ""
+                )
+                    return true;
+                if (
+                    (xDiff === 1 || xDiff === -1) &&
+                    yDiff === -1 &&
+                    toSpace.boardPiece !== ""
+                )
+                    return true;
+            } else {
+                // 6 is HARDCODED WONT WORK WITH DIFFERENT SIZED BOARDS
+                if (
+                    xDiff === 0 &&
+                    (yDiff === 1 || (fromSpace.boardY === 1 && yDiff === 2)) &&
+                    toSpace.boardPiece === ""
+                )
+                    return true;
+                if (
+                    (xDiff === 1 || xDiff === -1) &&
+                    yDiff === 1 &&
+                    toSpace.boardPiece !== ""
+                )
+                    return true;
+            }
+            // TODO: En passant
+            break;
+    }
     return true;
 };
